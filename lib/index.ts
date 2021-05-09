d5a88de93c64894e9a9c7b59610094eb79bca93e
soumik-mukherjee <60540758+soumik-mukherjee@users.noreply.github.com>
2021-05-09 21:36:23 +0530

    fixed export names

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,9 @@
-export * as vpc from "./vpc/default-vpc"
+export { DefaultVpcProps, DefaultVpc } from "./vpc/default-vpc"
+export {
+    CLUSTER_INSTANCE_SECURITY_GROUP_NAME,
+    DefaultVpcSecurityGroupsProps,
+    INTERFACE_ENDPOINTS_SECURITY_GROUP_NAME,
+    PUBLIC_LOAD_BALANCER_SECURITY_GROUP_NAME,
+    VPC_LINK_SECURITY_GROUP_NAME,
+    DefaultVpcSecurityGroups
+} from "./vpc/security-groups/default-vpc-sec-groups"
